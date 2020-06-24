e4b72e14bbbf317e76a9552bfbc44d5536a4a6eb
61246880 <jorge.plautz@gmail.com>
2020-06-24 14:20:22 -0300

    Criado o primeiro formulario (Form)
    close #16

--- myproject/settings.py
+++ myproject/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # apps. terceiros
+    'widget_tweaks',
     # apps. criadas
     'boards',
 ]
